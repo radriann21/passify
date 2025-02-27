a346ed2d2e5b59c6d6b758ae4edefabf6ba1aa1e
Adrian Rangel <adrianrangelargu@gmail.com>
2025-02-26 21:46:08 -0400

    feat: added github code link

--- src/app/components/HeaderComponent.tsx
+++ src/app/components/HeaderComponent.tsx
@@ -1,5 +1,7 @@
-import { Box, Flex, Heading, Mark } from "@chakra-ui/react";
+import { Box, Flex, Heading, Mark, Button, Separator, Icon } from "@chakra-ui/react";
+import { LuGithub } from "react-icons/lu";
 import { ColorModeButton } from "@/components/ui/color-mode";
+import Link from "next/link";
 
 export const HeaderComponent = () => {
   return (
@@ -9,12 +11,23 @@ export const HeaderComponent = () => {
       justifyContent="space-between"
     >
       <Box>
-        <Heading fontSize="1.5rem">
+        <Heading fontSize="1.5rem" fontWeight="bold">
           Pass
           <Mark color="blue.500" fontStyle="italic">ify</Mark>
         </Heading>
       </Box>
-      <ColorModeButton />
+      <Flex spaceX=".5rem">
+        <ColorModeButton />
+        <Separator orientation="vertical" />
+        <Link href="https://github.com/radriann21/passify" target="_blank" rel="noopener noreferrer">
+        <Button variant="ghost">
+          <Icon>
+            <LuGithub />
+          </Icon>
+          Code
+        </Button>
+        </Link>
+      </Flex>
     </Flex>
   )
 }
